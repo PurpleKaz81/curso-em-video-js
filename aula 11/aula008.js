ddf9322fd0b929e7074348f35dff42993c3f27cf
Rafael Kasinski <rakasin@gmail.com>
2022-10-07 17:41:25 -0300

    Finished ex008, with a simple if statement

---
+++ aula 11/aula008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/hr.`)
+if (vel > 60) { //simple condition, no else if
+  console.log(`Você ultrapassou a velocidade permitida, canalha! Multado!!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
