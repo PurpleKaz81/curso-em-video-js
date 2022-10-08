ef504bf3a9a290fa885472f2b002389fe12faca5
Rafael Kasinski <rakasin@gmail.com>
2022-10-08 02:49:32 -0300

    Fixed variable names and got image working with hours

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,14 @@ function load() {
   var msg = window.document.getElementById("msg")
   var image = window.document.getElementById("image")
   var date = new Date()
-  var time = date.getHours() + ":" + date.getMinutes()
+  var time = date.getHours()
+  // var time = 22
   msg.innerHTML = `Agora são ${time} horas.`
+  if (time >= 0 && time < 12) {
+    image.src = "manha.png"
+  } else if (time >= 12 && time < 18) {
+    image.src = "tarde.png"
+  } else {
+    image.src = "noite.png"
+  }
 }
