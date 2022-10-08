0d8b16792e466bc15a678d08adcee862e728eda5
Rafael Kasinski <rakasin@gmail.com>
2022-10-07 21:45:31 -0300

    Started building case arguments

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -8,3 +8,27 @@ var weekDay = now.getDay()
 */
 
 console.log(weekDay)
+
+switch(weekDay) {
+  case 0:
+    console.log(`Domingo`)
+    break
+  case 1:
+  console.log(`Segunda`)
+  break
+  case 2:
+  console.log(`Terça`)
+  break
+  case 3:
+  console.log(`Quarta`)
+  break
+  case 4:
+  console.log(`Quinta`)
+  break
+  case 5:
+  console.log(`Sexta`)
+  break
+  case 6:
+  console.log(`Sábado`)
+  break
+}
