cea56b84953321b5fe4e60a9a4e83d0bcff2d6c3
Rafael Kasinski <rakasin@gmail.com>
2022-10-07 21:09:59 -0300

    Finished voting conditionals

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,9 @@
-var idade = 22
-if (idade < 18) {
-  console.log(`Menor de idade.`)
+var idade = 67
+console.log(`Você tem ${idade} anos. Qual a natuireza do seu voto?`)
+if (idade < 16) {
+  console.log(`Indisponível.`)
+} else  if (idade < 18 || idade >= 65) { // idade >= 16 && is redundant{
+    console.log(`Optativo.`)
 } else {
-  console.log(`Maior de idade.`)
+    console.log(`Obrigatório.`)
 }
