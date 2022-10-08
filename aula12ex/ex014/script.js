c92fbb3945c5fd07c8bd617b2e5a2516efcbd57f
Rafael Kasinski <rakasin@gmail.com>
2022-10-08 02:35:57 -0300

    Created correct <section> message with JS

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,7 @@
-alert(`Olá!`)
+function load() {
+  var msg = window.document.getElementById("msg")
+  var image = window.document.getElementById("image")
+  var date = new Date()
+  var time = date.getHours() + ":" + date.getMinutes()
+  msg.innerHTML = `Agora são ${time} horas.`
+}
