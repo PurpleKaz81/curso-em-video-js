b4230a2e6dc3718169dddb0d4920f22dcfc416e8
Rafael Kasinski <rakasin@gmail.com>
2022-10-08 02:56:02 -0300

    Fixed background color to match with time of day

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,13 +3,16 @@ function load() {
   var image = window.document.getElementById("image")
   var date = new Date()
   var time = date.getHours()
-  // var time = 22
+  // var time = 21
   msg.innerHTML = `Agora são ${time} horas.`
   if (time >= 0 && time < 12) {
     image.src = "manha.png"
-  } else if (time >= 12 && time < 18) {
+    document.body.style.background = '#c5b5b6'
+  } else if (time >= 12 && time <= 18) {
     image.src = "tarde.png"
+    document.body.style.background = '#f6b456'
   } else {
     image.src = "noite.png"
+    document.body.style.background = '#a385bd'
   }
 }
