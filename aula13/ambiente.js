d4a175ceac07cba4c58d4eb80617a07e2024713c
Rafael Kasinski <rakasin@gmail.com>
2022-10-10 14:41:07 -0300

    Using do with while

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,14 +1,19 @@
 var counter = 1
-while (counter <= 6) {
-  console.log(`Passo ${counter}.ga`)
+do {
+  console.log(`Passo ${counter}.`)
   counter++
-}
+} while (counter <= 6)
 
 
 
 
 
 
+// var counter = 1
+// while (counter <= 6) {
+//   console.log(`Passo ${counter}.ga`)
+//   counter++
+// }
 
 // console.log(`Tudo bem?`)
 // console.log(`Tudo bem?`)
