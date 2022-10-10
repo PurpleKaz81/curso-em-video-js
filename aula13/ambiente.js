18e7aebab240441590ed212b60c64aaaa2dc059f
Rafael Kasinski <rakasin@gmail.com>
2022-10-10 14:36:03 -0300

    Using while loops in aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var counter = 1
+while (counter <= 6) {
+  console.log(`Passo ${counter}.ga`)
+  counter++
+}
+
+
+
+
+
+
+
+// console.log(`Tudo bem?`)
+// console.log(`Tudo bem?`)
+// console.log(`Tudo bem?`)
+// console.log(`Tudo bem?`)
+// console.log(`Tudo bem?`)
+// console.log(`Tudo bem?`)
