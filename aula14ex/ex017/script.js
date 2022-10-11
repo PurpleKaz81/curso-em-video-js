85a3a17b4352449a155896c750a6303abfa10de3
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 16:54:23 -0300

    Made adjustments with table.innerHTML

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -6,6 +6,7 @@ function check() {
   } else {
     let number = Number(inputNumber.value)
     let counter = 1
+    table.innerHTML = ""
     while (counter <= 10) {
       let item = window.document.createElement("option")
       item.text = `${number} x ${counter} = ${number*counter}`
