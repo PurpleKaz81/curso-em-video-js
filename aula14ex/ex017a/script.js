7e389bfb768bfd472b8a3d5d29a30d720962420d
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 18:22:42 -0300

    Change table for result.innerHTML

--- aula14ex/ex017a/script.js
+++ aula14ex/ex017a/script.js
@@ -1,14 +1,20 @@
 function calc() {
   let inputBase = window.document.querySelector("#base")
   let inputExponent = window.document.querySelector("#exponent")
-  let table = window.document.querySelector("#table")
+  let result = window.document.querySelector("#result")
   if (inputBase.value.length == 0 || inputExponent.value.length == 0) {
     window.alert("I need values, bruh... \u{1F644}")
   } else {
     let base = Number(inputBase.value)
     let exponent = Number(inputExponent.value)
     let counter = 1
-    table.innerHTML = ""
-    
+    result.innerHTML = ""
+    while (counter <= 10) {
+      let item = window.document.createElement("option")
+      item.text = `${base}^${exponent} = ${base**exponent}`
+      item.value = `table${counter}` //  useful for PHP
+      result.appendChild(item)
+      counter++
+    }
   }
 }
