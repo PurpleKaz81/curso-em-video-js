87f47a096de893abb04ca56dcb2ad722949ce074
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 13:33:58 -0300

    Fixed else part of the code, converting values to Numbers. Also put emokis in answer

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -10,18 +10,12 @@ function count() {
   } else if (end.value.length == 0) {
     result.innerHTML = "That's impossible to count."
   } else {
-    let counter = 0
-    let numbers = []
-    let element = start.value + step.value
-    while (counter < end.value) {
-      console.log(`${start.value} + ${step.value}`)
-      numbers.push(element)
-      counter += step.value
-      if (counter >= step.value)
-      console.log('That\'s it!')
-      break
+    result.innerHTML = "Contando: "
+    let firstNumber = Number(start.value)
+    let endNumber = Number(end.value)
+    let stepNumber = Number(step.value)
+    for (let counter = firstNumber;counter <= endNumber;counter += stepNumber) {
+      result.innerHTML += `${counter} + \u{1F449} `
     }
   }
 }
-
-result.innerHTML = numbers
