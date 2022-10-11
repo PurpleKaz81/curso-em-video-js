9f981030440c2f2162642a543a2838e7c3787cb5
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 16:35:11 -0300

    Rearranged conditional and blocks

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -2,8 +2,9 @@ function check() {
   let inputNumber = window.document.querySelector("#number")
   let table = window.document.querySelector("#table")
   if (inputNumber.value.length == 0) {
-    let number = Number(inputNumber.value)
-  } else {
     window.alert("I need a value, bruh... \u{1F644}")
+  } else {
+    let number = Number(inputNumber.value)
+
   }
 }
