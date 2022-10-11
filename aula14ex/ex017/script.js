f945ffde0b43725101d4adc750f6b850374c5a59
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 16:14:00 -0300

    Validation complete with warning

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,9 @@
-
+function check() {
+  let number = window.document.querySelector("#number")
+  let table = window.document.querySelector("#table")
+  if (number.value.length == 0) {
+    table.innerHTML = "I need a value, bruh... \u{1F644}"
+  } else {
+    
+  }
+}
