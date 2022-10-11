fd910778f53e2f7ae3c886fb68e540f050d98f1a
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 13:41:43 -0300

    Changed counting message and used inverse count, from larger to smaller number with steps

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -10,12 +10,18 @@ function count() {
   } else if (end.value.length == 0) {
     result.innerHTML = "That's impossible to count."
   } else {
-    result.innerHTML = "Contando: "
+    result.innerHTML = "Counting: "
     let firstNumber = Number(start.value)
     let endNumber = Number(end.value)
     let stepNumber = Number(step.value)
-    for (let counter = firstNumber;counter <= endNumber;counter += stepNumber) {
-      result.innerHTML += `${counter} \u{1F449} `
+    if (firstNumber < endNumber) {
+      for (let counter = firstNumber;counter <= endNumber;counter += stepNumber) {
+        result.innerHTML += `${counter} \u{1F449} `
+      }
+    } else {
+      for (let counter = firstNumber;counter >= endNumber;counter -= stepNumber) {
+        result.innerHTML += `${counter} \u{1F449} `
+      }
     }
     result.innerHTML += `\u{1F3C1}`
   }
