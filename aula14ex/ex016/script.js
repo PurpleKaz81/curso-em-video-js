23b3eac2312f4453b34a202c1ecf0397c261bca6
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 10:33:43 -0300

    Slowly building counter rules. Finished if and else if, moving to else

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,13 @@
-alert(`Olá!`)
+function count() {
+  var start = window.document.querySelector("#start")
+  var end = window.document.querySelector("#end")
+  var step = window.document.querySelector("#step")
+  var result = window.document.querySelector("div#result")
+  if (start.value.length == 0) {
+    result.innerHTML = "Can't count from naught, ol' love."
+  } else if (step.value.length == 0) {
+    window.alert("Can't count with zero steps.")
+  } else {
+    
+  }
+}
