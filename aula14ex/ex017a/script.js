ab52077d68694ffba11321dc7dd182b8c8eecde3
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 17:47:46 -0300

    Started else condition for table

--- aula14ex/ex017a/script.js
+++ aula14ex/ex017a/script.js
@@ -1,1 +1,11 @@
-
+function calc() {
+  let inputNumber = window.document.querySelector("#hypotenuse")
+  let table = window.document.querySelector("#table")
+  if (inputNumber.value.length == 0) {
+    window.alert("I need a value, bruh... \u{1F644}")
+  } else {
+    let number = Number(inputNumber.value)
+    let counter = 1
+    table.innerHTML = ""
+  }
+}
