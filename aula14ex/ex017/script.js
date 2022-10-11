9e911426971763dd9ff009ed095808a07e3a0403
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 16:47:44 -0300

    Put in counter++, which I'd forgotten. Now while loop works

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -5,6 +5,12 @@ function check() {
     window.alert("I need a value, bruh... \u{1F644}")
   } else {
     let number = Number(inputNumber.value)
-
+    let counter = 1
+    while (counter <= 10) {
+      let item = window.document.createElement("option")
+      item.text = `${number} x ${counter} = ${number*counter}`
+      table.appendChild(item)
+      counter++
+    }
   }
 }
