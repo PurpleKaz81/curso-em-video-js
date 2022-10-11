25b36f060ca6f54c6cc4ca385bc7e37a1a0dade3
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 15:37:21 -0300

    Fixed validations

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,22 +3,20 @@ function count() {
   let end = window.document.querySelector("#end")
   let step = window.document.querySelector("#step")
   let result = window.document.querySelector("div#result")
-  if (start.value.length == 0) {
-    result.innerHTML = "Can't count from naught, ol' love."
-  } else if (step.value.length == 0 || step.value == 0) {
-    window.alert("Can't count with zero steps.")
-  } else if (end.value.length == 0) {
-    result.innerHTML = "That's impossible to count."
+  if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0) {
+    result.innerHTML = "[ERROR] Can't count like that, ol' love."
   } else {
     result.innerHTML = "Counting: \u{1F3C1} "
     let firstNumber = Number(start.value)
     let endNumber = Number(end.value)
     let stepNumber = Number(step.value)
     if (firstNumber < endNumber) {
+      // Ascending count
       for (let counter = firstNumber;counter <= endNumber;counter += stepNumber) {
         result.innerHTML += `${counter} \u{1F449} `
       }
     } else {
+      // Descending count
       for (let counter = firstNumber;counter >= endNumber;counter -= stepNumber) {
         result.innerHTML += `${counter} \u{1F449} `
       }
