b3d3fa656c87421aff4f160ed3b2b7ad69118289
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 18:10:38 -0300

    Made adjustments in CSS and HTML

--- aula14ex/ex017a/script.js
+++ aula14ex/ex017a/script.js
@@ -1,10 +1,12 @@
 function calc() {
-  let inputNumber = window.document.querySelector("#hypotenuse")
+  let inputBase = window.document.querySelector("#base")
+  let inputExponent = window.document.querySelector("#exponent")
   let table = window.document.querySelector("#table")
-  if (inputNumber.value.length == 0) {
-    window.alert("I need a value, bruh... \u{1F644}")
+  if (inputBase.value.length == 0 || inputExponent.value.length == 0) {
+    window.alert("I need values, bruh... \u{1F644}")
   } else {
-    let number = Number(inputNumber.value)
+    let base = Number(inputBase.value)
+    let exponent = Number(inputExponent.value)
     let counter = 1
     table.innerHTML = ""
     
