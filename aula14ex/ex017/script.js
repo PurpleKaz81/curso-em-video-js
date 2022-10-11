59463b172359f049d68c19529dc1f6ac01a35811
Rafael Kasinski <rakasin@gmail.com>
2022-10-11 16:20:43 -0300

    Started else condition

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -4,6 +4,6 @@ function check() {
   if (number.value.length == 0) {
     table.innerHTML = "I need a value, bruh... \u{1F644}"
   } else {
-    
+    table.innerHTML = `Multiplication Table for ${number.value}`
   }
 }
