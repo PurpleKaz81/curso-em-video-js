cde8864decc151074c55f97a93dc4b21ef8868f6
Rafael Kasinski <rakasin@gmail.com>
2022-10-19 15:21:26 -0300

    Slight changes in messages

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,10 +4,12 @@ console.log(num)
 
 console.log(`Nosso vetor é o ${num}`)
 
-console.log(`O vetor tem ${num.length} elementos`)
+console.log(`Ele tem ${num.length} elementos`)
 
 console.log(`O primeiro valor deste array é ${num[0]}`)
 
 num.push(9)
 
-console.log(`O novo valor deste vetor é o ${num.sort()[5]}`)
+num
+
+console.log(`Agora, seu novo valor é ${num.sort()[5]}`)
