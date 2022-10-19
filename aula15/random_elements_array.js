74f366db0da9b635a1b87d2696500cacfc8c1355
Rafael Kasinski <rakasin@gmail.com>
2022-10-19 18:00:25 -0300

    Figured out how to avoid 0 in the array's elements

--- aula15/random_elements_array.js
+++ aula15/random_elements_array.js
@@ -2,7 +2,7 @@
 
 // Method 1
 
-let array1 = Array.from({length: 40}, () => Math.floor(Math.random() * 40)).sort()
+let array1 = Array.from({length: 40}, () => Math.floor(Math.random() * 40) + 1).sort()
 
 array1
 
@@ -13,10 +13,10 @@ while(array2.length < 40){
     let random = Math.floor(Math.random() * 100) + 1;
     if(array2.indexOf(random) === -1) array2.push(random);
 }
-console.log(array2);
+console.log(array2.sort());
 
 // Method 3
 
-const numbers = Array(100).fill().map((_, index) => index + 1);
-numbers.sort(() => Math.random() - 0.5);
-console.log(numbers.slice(0, 10));
+let array3 = Array(100).fill().map((_, index) => index + 1);
+array3.sort(() => Math.random() - 0.5);
+console.log(array3.slice(0, 10).sort());
