ea15ec01b6fc9b303aea9ec578edefffa9e7025f
Rafael Kasinski <rakasin@gmail.com>
2022-10-19 17:13:45 -0300

    Fixed issue with sort()

--- aula15/vetor_na_tela.js
+++ aula15/vetor_na_tela.js
@@ -1,13 +1,28 @@
 let valores = [8, 1, 7, 4, 2, 9]
-
 console.log(valores)
-console.log(valores[0])
+valores.push(56)
+console.log(valores)
 
-for (let pos = 0; pos < valores.length; pos++) {
-  let el = valores[pos];
-  console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
-}
+valores.sort()
 
-for (let pos in valores) {
-  console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
-}
+// console.log(valores)
+// console.log(valores[0])
+
+// for (let pos = 0; pos < valores.length; pos++) {
+  //   let el = valores[pos];
+  //   console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
+  // }
+
+  for (let pos in valores) {
+    console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
+  }
+
+  console.log(valores.indexOf(2))
+
+  valores.sort((a, b) => a - b)
+
+  for (let pos in valores) {
+    console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
+  }
+
+  console.log(valores.indexOf(2))
