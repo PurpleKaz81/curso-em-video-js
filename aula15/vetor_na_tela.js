ce7c5070337a666875ec4de0d44aa90756770a11
Rafael Kasinski <rakasin@gmail.com>
2022-10-19 15:52:08 -0300

    Finished for loop

--- aula15/vetor_na_tela.js
+++ aula15/vetor_na_tela.js
@@ -1,2 +1,9 @@
-let valores = []
+let valores = [8, 1, 7, 4, 2, 9]
 
+console.log(valores)
+console.log(valores[0])
+
+for (let pos = 0; pos < valores.length; pos++) {
+  let el = valores[pos];
+  console.log(`A posição ${pos} tem valor de ${valores[pos]}`)
+}
