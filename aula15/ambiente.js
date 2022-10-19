0bd67498f388361426853bcf57e58bb87b09dd55
Rafael Kasinski <rakasin@gmail.com>
2022-10-19 15:18:12 -0300

    Doing basic functions in JS using arrays

---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+
+console.log(num)
+
+console.log(`Nosso vetor é o ${num}`)
+
+console.log(`O vetor tem ${num.length} elementos`)
+
+console.log(`O primeiro valor deste array é ${num[0]}`)
+
+num.push(9)
+
+console.log(`O novo valor deste vetor é o ${num.sort()[5]}`)
