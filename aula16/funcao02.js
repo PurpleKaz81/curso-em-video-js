d8485cbd0afcff4af1a67ed94f63aed481d35faf
Rafael Kasinski <rakasin@gmail.com>
2022-10-20 15:41:07 -0300

    Finished second exercise for aula16

---
+++ aula16/funcao02.js
@@ -0,0 +1,9 @@
+function sum(n1=0, n2=0) { // put n=0 for a predefined value in case of no input in function call below
+  return n1 + n2
+}
+
+let result1 = sum(2, 7)
+result1
+
+let result2 = sum(7)
+result2
