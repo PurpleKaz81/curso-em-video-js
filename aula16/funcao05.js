9554bbe4373681ce85384ca71317a7a205b46170
Rafael Kasinski <rakasin@gmail.com>
2022-10-20 16:44:15 -0300

    Refactored funcao05.js

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -1,12 +1,12 @@
 function factorial(number) {
-  if (number == 1) {
+  if (number == 1 || number == 0) {
     return 1
   } else {
     return number * factorial(number - 1)
   }
 }
 
-console.log(factorial(5))
+console.log(factorial(0))
 
 /* recursive fashion
 
