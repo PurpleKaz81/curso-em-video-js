d7b3a8b91efeea44bdb132d44218eaa88e8b2ca9
Rafael Kasinski <rakasin@gmail.com>
2022-10-20 16:42:21 -0300

    Refactored funcao06extra.js

--- aula16/funcao06extra.js
+++ aula16/funcao06extra.js
@@ -5,8 +5,7 @@ let factorial = (x) => {
   if (x % 1 !== 0) {
     x = parseInt(x)
   }
-  if (x === 0) return 1
-  if (x === 1) return 1
+  if (x === 0 || x === 1) return 1
   return x * factorial(x - 1)
 }
 
