2410dfe0686f35c13f15070b0cd2a142fb859dd5
Rafael Kasinski <rakasin@gmail.com>
2022-10-21 00:43:58 -0300

    Finished first part, with input for numbers and button, as well as table and button for analyze

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,3 +1,14 @@
-function input(params) {
-  
+function input() {
+  let inputNumber = window.document.querySelector("#number")
+  let input = window.document.querySelector("input")
+  if (inputNumber.value.length === 0) {
+    window.alert("I need a value, bruh \u{1F913}")
+  }
+  let number = Number(inputNumber.value)
+  let item = window.document.createElement("option")
+  // table.innerHTML = ""
+  if (number !== 0) {
+    item.text = `You've added the number ${number}`
+	  table.appendChild(item)
+  }
 }
