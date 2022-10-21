e2a9ffa4ca0f602ae8e6ef70fcf6304606492ef7
Rafael Kasinski <rakasin@gmail.com>
2022-10-21 16:55:34 -0300

    Fixed HTML section and working on finalizar() function

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -31,3 +31,14 @@ function adicionar() {
   num.value = ""
   num.focus()
 }
+
+function finalizar() {
+  if (valores.length == 0) {
+    window.alert("Adicione valores antes de finalizar 🙃")
+  } else {
+    let tot = valores.length
+
+    res.innerHTML = ""
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+  }
+}
